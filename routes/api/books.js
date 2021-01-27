15e90ee2507df7f5434c19c18d1bc7c690f47a8c
Jonah Mallard <benjaminjmallard@gmail.com>
2021-01-27 16:06:01 -0600

    Edit a book page

--- routes/api/books.js
+++ routes/api/books.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 var booksCtrl = require('../../controllers/api/books');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
+const ensureLoggedInUser = require('../../config/ensureLoggedInUser');
 
 /* GET /api/books */
 router.get('/', booksCtrl.index);
-router.post('/', ensureLoggedIn, booksCtrl.create);
-router.get('/:id', ensureLoggedIn, booksCtrl.show);
-router.put('/:id', ensureLoggedIn, booksCtrl.update);
-router.delete('/:id', ensureLoggedIn, booksCtrl.delete)
+router.post('/', booksCtrl.create);
+router.get('/:id', booksCtrl.show);
+router.put('/:id', booksCtrl.update);
+router.delete('/:id', booksCtrl.delete)
 
 module.exports = router;
